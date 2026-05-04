3c76e943f11fd0b85dfe115dfc8e8deb73f0b6f1
0HOVOH0 <143569594+0HOVOH0@users.noreply.github.com>
2026-05-04 22:26:45 +0800

    Load photo and Notion sync fix on Netlify

--- patch-netlify.js
+++ patch-netlify.js
@@ -5,5 +5,10 @@ let html = fs.readFileSync(htmlPath, "utf8");
 
 if (!html.includes("enhancements.js")) {
   html = html.replace("</body>", '<script src="./enhancements.js"></script>\n</body>');
-  fs.writeFileSync(htmlPath, html);
 }
+
+if (!html.includes("photo-notion-fix.js")) {
+  html = html.replace("</body>", '<script src="./photo-notion-fix.js"></script>\n</body>');
+}
+
+fs.writeFileSync(htmlPath, html);
